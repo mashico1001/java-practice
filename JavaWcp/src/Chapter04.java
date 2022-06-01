6be10a923e8744e39d5d3d4262eb8c4dbaed9917
mashico1001 <92191079+mashico1001@users.noreply.github.com>
2022-06-01 17:05:12 +0900

    [Add]JavaChapter5完了

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,13 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String str1 = "Shimazaki Yuko";
+		System.out.println(str1);
+		str1 = "少年よ大志をいだけ";
+		System.out.println(str1);
+		
+		final int number = 7;
+		System.out.println(number);
+	}
+
+}
