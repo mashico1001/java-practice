75463b592c376a2236d3b4bf694fff07ff458b58
mashico1001 <92191079+mashico1001@users.noreply.github.com>
2022-06-08 15:12:01 +0900

    [Finish]98

---
+++ Java_sukkiri/src/Hero.java
@@ -0,0 +1,41 @@
+
+public class Hero {
+	public static void main(String[] args) {
+//		int a = 3;
+//		int b = 5;
+//		int c = (a * b);
+//		
+//		System.out.println(c);
+//		
+//		int m = Math.max(a, b);
+//		System.out.println("比較実験：" + a + "と" + b + "とで大きい方は・・・" + m);
+//		
+//		boolean boo = true;
+//		char kanji = '駆';
+//		double d = 3.14;
+//		long l = 314159265853979L;
+//		String message = "ミナトの攻撃！敵に15ポイントのダメージを与えた！";
+//		
+//		System.out.println(boo);
+//		System.out.println(kanji);
+//		System.out.println(d);
+//		System.out.println(l);
+//		System.out.println(message);
+//		
+//		int age = (int)3.2;
+//		System.out.println(age);
+		
+//		System.out.println("あなたの名前を入力してください");
+//		String name = new java.util.Scanner(System.in).nextLine();
+//		System.out.println("あなたの年齢を入力してください");
+//		int age = new java.util.Scanner(System.in).nextInt();
+//		System.out.println("ようこそ" + age + "歳の" + name + "さん");
+		
+		int x = 5;
+		int y = 10;
+		String ans = "x+yは" + (x + y);
+		System.out.println(ans);
+		
+	}
+
+}
