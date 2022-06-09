556b9c408c566e4d97f9324c70c7a8d44bd38b01
mashico1001 <92191079+mashico1001@users.noreply.github.com>
2022-06-09 16:44:22 +0900

    [Finish]Chapter6

---
+++ Java_sukkiri/src/comment/Kouhan.java
@@ -0,0 +1,15 @@
+package comment;
+
+public class Kouhan {
+	public static void callDeae() {
+		System.out.println("ええい、こしゃくな。曲者だ、であえ！");
+	}
+	
+	public static void showMondokoro() throws Exception {
+		System.out.println("飛車さん、角さん。もういいでしょう。");
+		System.out.println("この紋所が目にはいらぬか！");
+		Thread.sleep(3000);
+		Zenhan.doTogame();
+	}
+
+}
